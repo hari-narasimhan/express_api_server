740f272e979fbbe480c64ae69fc15e99e73e2730
Hari Narasimhan <001116@q4-chn-l-168.chennai.qfor.com>
2016-07-07 16:07:43 +0530

    references to db removed

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var db = require('./db');
 
 var app = express();
-
-// Initialize Database
-db.init();
 
 // extended: true allows rich query string to be parsed
 app.use(bodyParser.json());
